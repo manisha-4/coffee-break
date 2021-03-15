548c217d121fd8b38732e020bbdc5769d6067697
manisha-4 <aadi8910sha@gmail.com>
2021-03-15 20:58:28 +0530

    home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/screens/home/brewlist.dart';
+import 'package:brew_crew/screens/home/mainDrawer.dart';
 import 'package:brew_crew/screens/home/settings_form.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/services/database.dart';
@@ -20,49 +21,52 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-     void _showSettingsPanel() {
-      showModalBottomSheet(context: context, builder: (context) {
-        return Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-          child: SettingsForm(),
-        );
-      });
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: SettingsForm(),
+            );
+          });
     }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
           title: Text('Brew Crew'),
-          // centerTitle: true,
+           centerTitle: true,
           backgroundColor: Colors.brown[400],
           elevation: 0.0,
           actions: <Widget>[
+            
+           // FlatButton.icon(
+             // icon: Icon(Icons.person),
+              //label: Text('logout'),
+              //onPressed: () async {
+                //await _auth.signOut();
+              //},),
             FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            ),
- FlatButton.icon(
               icon: Icon(Icons.settings),
               label: Text('settings'),
               onPressed: () => _showSettingsPanel(),
             )
-
-
           ],
         ),
+
+        drawer:MainDrawer(),
         body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/coffee_bg.png'),
-              fit: BoxFit.cover,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/coffee_bg.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: BrewList()
-        ),
+            child: BrewList()
+            ),
       ),
     );
   }
